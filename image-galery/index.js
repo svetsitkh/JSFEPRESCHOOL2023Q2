072d836e0fd727bcbad4c3c6a4ee854bf269d245
svetsitkh <svetsitkh@gmail.com>
2023-10-02 00:14:10 +0300

    feat: implement search and add footer (Mon, Oct 02, 2023 12:14:10 AM)

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,25 +1,69 @@
-let url = 'https://api.unsplash.com/search/photos?query=waterfall&per_page=30&orientation=landscape&client_id=ys_OSYX6mtTO91uGO1x_cMkDNKeJb0Z8OdYs_PI07_g';
+const urlDefault = 'https://api.unsplash.com/search/photos?query=waterfall&per_page=30&orientation=landscape&client_id=ys_OSYX6mtTO91uGO1x_cMkDNKeJb0Z8OdYs_PI07_g';
 const searchBtn = document.querySelector('.search_btn');
+const searchInput = document.querySelector('.search_input');
 const gridContainer = document.querySelector('.grid_container');
+const form = document.querySelector('form');
 
-async function fetchImages () {
+let arrayOfImages = [];
+
+async function fetchImages (url) {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        data.results.forEach(el => {
-            const imgSrc = el.urls.regular;
-            console.log(imgSrc);
-            const img = document.createElement('img');
-            img.classList.add('image');
-            img.src = imgSrc;
-            img.alt = 'Picture';
-            gridContainer.append(img);
-        });
+        return data.results;
     } catch (error) {
         console.log(error);
+        return [];
     }
 }
 
-fetchImages();
+function displayImages(arrayImg) {
+    arrayOfImages = arrayImg.map(el => {
+        const imgSrc = el.urls.regular;
+        const img = document.createElement('img');
+        img.classList.add('image');
+        img.src = imgSrc;
+        img.alt = 'Picture';
+        gridContainer.append(img);
 
-searchBtn.addEventListener('click', fetchImages);
+        return {
+            full: el.urls.full,
+            regular: el.urls.regular
+        }
+    });
+}
+
+async function fetchDisplayImages () {
+    let actualUrl = '';
+    const requestText = searchInput.value;
+    console.log(requestText);
+    if (requestText.trim() == '') {
+        actualUrl = urlDefault;
+    } else {
+        actualUrl = `https://api.unsplash.com/search/photos?query=${requestText}&per_page=30&orientation=landscape&client_id=ys_OSYX6mtTO91uGO1x_cMkDNKeJb0Z8OdYs_PI07_g`;
+    }
+
+    const fetchedImages = await fetchImages(actualUrl);
+    displayImages(fetchedImages);
+
+    console.log(arrayOfImages);
+}
+
+function removeImages() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function onClickSearchBtn () {
+    removeImages();
+    fetchDisplayImages();
+}
+
+document.addEventListener("DOMContentLoaded", fetchDisplayImages);
+
+searchBtn.addEventListener('click', onClickSearchBtn);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onClickSearchBtn();
+})
